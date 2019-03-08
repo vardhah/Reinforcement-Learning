0936651fee95617e8554ef714c40029a6bf97948
vardhah <38508679+vardhah@users.noreply.github.com>
2019-03-08 03:57:56 +0000

    working 1 & 2

--- value_iteration.py
+++ value_iteration.py
@@ -2,34 +2,57 @@
 """
 Created on Mon Feb 18 16:31:23 2019
 
-@author: HPP
+@author: Harsh Vardhan 
 """
 
 import numpy as np
 import matplotlib as plt
 import sklearn as sk
 
-r=-1
-gamma=1
-v=np.zeros((4,4))
-delta=0
-print(v.shape)
 v_old=np.zeros((4,4))
-for i in range(10):
+
+def value_iteration(iterator=10):
+ r=-1
+ gamma=1
+ global v_old
+ v=np.zeros((4,4))
+ #delta=0
+ print(v.shape)
+ v_old=np.zeros((4,4))
+ for i in range(iterator):
     v_old[:]=v
-    print('v_old:\n', v_old)
-    v[1]=(1/4)*(r+gamma*v_old[0])+(1/4)*(r+gamma*v_old[2])+(1/4)*(r+gamma*v_old[5])+(1/4)*(r+gamma*v_old[1])
-    v[2]=(1/4)*(r+gamma*v_old[1])+(1/4)*(r+gamma*v_old[3])+(1/4)*(r+gamma*v_old[6])+(1/4)*(r+gamma*v_old[2])
-    v[3]=(1/4)*(r+gamma*v_old[2])+(1/4)*(r+gamma*v_old[3])+(1/4)*(r+gamma*v_old[7])+(1/4)*(r+gamma*v_old[3])
-    v[4]=(1/4)*(r+gamma*v_old[4])+(1/4)*(r+gamma*v_old[5])+(1/4)*(r+gamma*v_old[0])+(1/4)*(r+gamma*v_old[8])
-    v[5]=(1/4)*(r+gamma*v_old[4])+(1/4)*(r+gamma*v_old[6])+(1/4)*(r+gamma*v_old[1])+(1/4)*(r+gamma*v_old[9])
-    v[6]=(1/4)*(r+gamma*v_old[5])+(1/4)*(r+gamma*v_old[7])+(1/4)*(r+gamma*v_old[2])+(1/4)*(r+gamma*v_old[10])
-    v[7]=(1/4)*(r+gamma*v_old[6])+(1/4)*(r+gamma*v_old[7])+(1/4)*(r+gamma*v_old[3])+(1/4)*(r+gamma*v_old[11])
-    v[8]=(1/4)*(r+gamma*v_old[8])+(1/4)*(r+gamma*v_old[9])+(1/4)*(r+gamma*v_old[4])+(1/4)*(r+gamma*v_old[12])
-    v[9]=(1/4)*(r+gamma*v_old[8])+(1/4)*(r+gamma*v_old[10])+(1/4)*(r+gamma*v_old[5])+(1/4)*(r+gamma*v_old[13])
-    v[10]=(1/4)*(r+gamma*v_old[9])+(1/4)*(r+gamma*v_old[11])+(1/4)*(r+gamma*v_old[6])+(1/4)*(r+gamma*v_old[14])
-    v[11]=(1/4)*(r+gamma*v_old[10])+(1/4)*(r+gamma*v_old[11])+(1/4)*(r+gamma*v_old[7])+(1/4)*(r+gamma*v_old[15])
-    v[12]=(1/4)*(r+gamma*v_old[12])+(1/4)*(r+gamma*v_old[13])+(1/4)*(r+gamma*v_old[8])+(1/4)*(r+gamma*v_old[12])
-    v[13]=(1/4)*(r+gamma*v_old[12])+(1/4)*(r+gamma*v_old[14])+(1/4)*(r+gamma*v_old[9])+(1/4)*(r+gamma*v_old[13])
-    v[14]=(1/4)*(r+gamma*v_old[13])+(1/4)*(r+gamma*v_old[15])+(1/4)*(r+gamma*v_old[10])+(1/4)*(r+gamma*v_old[14])
-    print('v is:',v)
+    for i in range(v.shape[0]):
+        for j in range(v.shape[1]):
+          if (i==0 and j==0):
+              continue
+              print(v[i][j])
+          if(i==3 and j==3):
+              continue
+              print(v[i][j])
+          else:
+            v[i][j]= 1/4*(r+gamma*rightV(i,j))+1/4*(r+gamma*leftV(i,j))+1/4*(r+gamma*upV(i,j))+1/4*(r+gamma*downV(i,j))
+    print('v is:\n',v)
+
+def rightV(x,y):
+    if(y!=3):
+           y+=1
+    return v_old[x][y]
+
+def leftV(x,y):
+    if(y!=0):
+           y-=1 
+    return v_old[x][y]
+    
+def upV(x,y):
+    if(x!=0):
+           x-=1 
+    return v_old[x][y]
+    
+def downV(x,y):
+    if(x!=3):
+           x+=1   
+    return v_old[x][y]
+   
+    
+   
+    
